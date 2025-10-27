501ee5845f879f1291a711ddc1adcfc3f9725a51
VINICIUS JULIO DE OLIVEIRA <25132898@CFP127ALUNO.LOCAL>
2025-10-27 16:54:20 -0300

    pagina 1 Iniciada

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
 'use strict'
 
+
+const message_erro = document.getElementById('erro')
+
 async function buscarTodosUsuario(numero) {
-    const response = await fetch(`https://api-zaz.onrender.com/v1/whatzapp/usuario/${numero}`)
-    const dados = await response.json()
-    return dados
+    try {
+        const response = await fetch(`https://api-zaz.onrender.com/v1/whatzapp/usuario/${numero}`)
+        const dados = await response.json()
+        console.log(dados)
+        return dados
+
+    } catch (erro) {
+        console.error('Erro ao buscar usuário:', erro)
+        return false
+    }
 }
 
 async function informacaoUsuario(numero) {
-    const usuario = buscarTodosUsuario(numero)
 
-    if (numero == usuario.perfil.numero) {
-        
-        return true
-    } else {
-        return false
+    const numeros = numero
+
+    botaoEntra.disabled = true
+
+    const usuario = await buscarTodosUsuario(numeros)
+
+    botaoEntra.disabled = false
+
+    if (usuario == false) {
+        message_erro.textContent = 'Numero inválido!'
+    }
+
+    if (numeros === usuario.perfil.numero) {
+
+        localStorage.setItem('numeroValido', JSON.stringify(numero))
+        window.location.href = './page1/page1.html'
     }
 }
 
 const botaoEntra = document.getElementById('entra')
 botaoEntra.addEventListener('click', async () => {
-    informacaoUsuario()
-    
-} )
+    const numero = inputNumero.value
+
+
+    if (numero === '') {
+
+        message_erro.textContent = 'Digite um Número antes de continuar'
+        return
+
+    }
+    message_erro.textContent = ''
+    await informacaoUsuario(numero)
+
+})
 const inputNumero = document.getElementById('inputNumero')
 inputNumero.addEventListener('input', () => {
-    inputNumero.value = inputNumero.value.replace(/[^0-9]/g ,'')
+    inputNumero.value = inputNumero.value.replace(/[^0-9]/g, '')
 })
-buscarTodosUsuario()
---
+++ page1/page1.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const numeros = JSON.parse(localStorage.getItem('numeroValido'))
+console.log(numeros)
+
+async function buscarTodosUsuario(numero) {
+    try {
+        const response = await fetch(`https://api-zaz.onrender.com/v1/whatzapp/usuario/contatos/${numero}`)
+        const dados = await response.json()
+        console.log(dados)
+        return dados
+
+    } catch (erro) {
+        console.error('Erro ao buscar usuário:', erro)
+        return false
+    }
+}
+
+async function exibirContatos(numeros) {
+
+    const contato = buscarTodosUsuario(numeros)
+
+    const main = document.getElementById('main')
+
+    const containerContato = document.createElement('div')
+    containerContato.className = 'contato'
+
+    let arrayContatos = await contato
+
+    arrayContatos.contatos.forEach(cont =>  {
+        let img = document.createElement('img')
+        img.src = `../img_contatos/${cont.imagem}`
+
+        let containerInfo = document.createElement('div')
+        containerInfo.className = 'informacoes_contato'
+
+        let nome = document.createElement('h3')
+        nome.className = 'nome_contato'
+        nome.textContent = cont.nome
+
+        let descricao = document.createElement('p')
+        descricao.className = 'descricao_contato'
+        descricao.textContent = cont.descricao
+
+        containerInfo.append(nome, descricao)
+        containerContato.append(img, containerInfo)
+        
+
+
+    })
+    main.append(containerContato)
+
+
+}
+exibirContatos(numeros)
+
